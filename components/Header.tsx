887d71c8799689a4b8b54ba09030fe99a1ad1dd3
rosarie <rosariosanchezsampietro@gmail.com>
2023-01-26 20:48:12 -0300

    Header styles motion and icon links

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,73 @@
 import React from "react";
+import { SocialIcon } from "react-social-icons";
+import { motion } from "framer-motion";
 
 type Props = {};
 
 export default function Header({}: Props) {
   return (
-    <header>
-      <div>Header</div>
+    <header className="sticky top-0 p-5 flex flex-items-start justify-between max-w-7xl mx-auto z-20 xl:items-center">
+      <motion.div
+        initial={{
+          x: -500,
+          opacity: 0,
+          scale: 0.5,
+        }}
+        animate={{
+          x: 50,
+          opacity: 1,
+          scale: 1,
+        }}
+        transition={{
+          duration: 1.5,
+        }}
+        className="flex flex-row items-center"
+      >
+        <SocialIcon
+          url="https://www.linkedin.com/in/rosariosanchezsampietro/"
+          fgColor="grey"
+          bgColor="transparent"
+          target="_blank"
+        />
+        <SocialIcon
+          url="https://www.github.com/argrosarie"
+          fgColor="grey"
+          bgColor="transparent"
+          target="_blank"
+        />
+        <SocialIcon
+          url="https://twitter.com/argrosarie"
+          fgColor="grey"
+          bgColor="transparent"
+          target="_blank"
+        />
+      </motion.div>
+      <motion.div
+        initial={{
+          x: 500,
+          opacity: 0,
+          scale: 0.5,
+        }}
+        animate={{
+          x: 0,
+          opacity: 1,
+          scale: 1,
+        }}
+        transition={{
+          duration: 1.5,
+        }}
+        className="flex flex-row items-center text-gray-300 cursor-pointer"
+      >
+        <SocialIcon
+          className="cursor-pointer"
+          network="email"
+          fgColor="grey"
+          bgColor="transparent"
+        />
+        <p className="uppercase hidden md:inline-flex text-sm text-gray-400">
+          Get it touch
+        </p>
+      </motion.div>
     </header>
   );
 }
