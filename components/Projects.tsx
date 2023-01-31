aff720f1522da2fb2c3e340e682254f35f1093a8
rosarie <rosariosanchezsampietro@gmail.com>
2023-01-31 18:21:27 -0300

    Motion framer for Projects component

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,17 +1,67 @@
-import React from 'react'
+import React from "react";
+import { motion } from "framer-motion";
 
-type Props = {}
+type Props = {};
 
 function Projects({}: Props) {
+  const projects = [1, 2, 3, 4, 5, 6];
   return (
-    <div className='h-screen relative flex overflow-hidden flex-col text-left md:flex-row max-w-full justify-evenly mx-auto items-center z-0'>
-        <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>Projects</h3>
-        <div className='relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20'>
-            {/* projects */}
-        </div>
-        <div className='w-full absolute top-[30%] bg-[#F7AB0A]/10 left-0 h-[500px] -skew-y-12'/>
-    </div>
-  )
+    <motion.div 
+    initial={{
+        opacity:0
+    }}
+    whileInView={{
+        opacity:1
+    }}
+    transition={{
+        duration: 1.2
+    }}
+    className="h-screen relative flex overflow-hidden flex-col text-left md:flex-row max-w-full justify-evenly mx-auto items-center z-0">
+      <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
+        Projects
+      </h3>
+      <div className="relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20">
+        {projects.map((project) => (
+          // eslint-disable-next-line react/jsx-key
+          <div className="w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-20 md:p-44 h-screen">
+            <motion.img
+            initial={{
+                y: -300,
+                opacity:0
+            }}
+            transition={{
+                duration: 1.2
+            }}
+            whileInView={{
+                opacity:1,
+                y:0
+            }}
+            viewport={{
+                once: true
+            }}
+              src="https://cdn.sanity.io/images/ltuexkre/production/b54b0a9525e943809d5cfd9b1549b9f7d3b8858f-500x500.png"
+              alt=""
+            />
+            <div className="space-x-10 px-0 md:px-10 max-w-6xl">
+              <h4 className="text-4xl font-semibold text-center">
+                <span className="underline decoration-[#F7AB0A]/50">
+                  {" "}
+                  Lorem ipsum{" "}
+                </span>
+                dolorg elit
+              </h4>
+              <p className="text-lg text-center md:text-left">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Asperiores perferendis laboriosam nostrum facilis earum aliquid
+                dolores.
+              </p>
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className="w-full absolute top-[30%] bg-[#F7AB0A]/10 left-0 h-[500px] -skew-y-12" />
+    </motion.div>
+  );
 }
 
-export default Projects
+export default Projects;
