ea4c3c69de2a935d300cbf775e4ec85da19ebe3a
rosarie <rosariosanchezsampietro@gmail.com>
2023-01-31 15:38:16 -0300

    Created Projects component

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,7 @@ import Hero from "@/components/Hero";
 import About from "../components/About";
 import Experience from "../components/Experience";
 import Skills from "../components/Skills";
+import Projects from "../components/Projects";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -27,6 +28,9 @@ export default function Home() {
       </section>
       <section id="skills" className="snap-start">
         <Skills />
+      </section>
+      <section id="projects" className="snap-start">
+        <Projects />
       </section>
     </div>
   );
