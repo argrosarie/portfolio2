45bf54b3ac47ab50efc91ac420d05ed72752c970
rosarie <rosariosanchezsampietro@gmail.com>
2023-01-31 15:02:01 -0300

    Added Skill and Skills components

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 import About from "../components/About";
 import Experience from "../components/Experience";
+import Skills from "../components/Skills";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -23,6 +24,9 @@ export default function Home() {
       </section>
       <section id="experience" className="snap-center">
         <Experience />
+      </section>
+      <section id="skills" className="snap-start">
+        <Skills />
       </section>
     </div>
   );
