e45dbb05cdc9c1c2d186620aeeb61d7486cdd866
rosarie <rosariosanchezsampietro@gmail.com>
2023-02-01 13:29:27 -0300

    Scrollbar Index and Experience components

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,7 +13,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0">
+    <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0 scrollbar scrollbar-track-gray-400/20  scrollbar-thumb-[#F7AB0A]/80">
       <Head>
         <title>Rosario's portfolio</title>
       </Head>
