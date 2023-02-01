2ef1682674d7af043961721745e7ab1a88edc51a
rosarie <rosariosanchezsampietro@gmail.com>
2023-02-01 13:35:50 -0300

    Modified scrollbars

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -20,7 +20,7 @@ function Projects({}: Props) {
       <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
         Projects
       </h3>
-      <div className="relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20">
+      <div className="relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20 scrollbar-thin scrollbar-track-gray-400/20  scrollbar-thumb-[#F7AB0A]/80">
         {projects.map((project) => (
           // eslint-disable-next-line react/jsx-key
           <div className="w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-20 md:p-44 h-screen">
