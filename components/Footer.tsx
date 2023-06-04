a5bc9df53175092e7d6d157bace7ce6d6c795afb
rosarie <rosariosanchezsampietro@gmail.com>
2023-06-04 20:59:21 -0300

    Gay footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -26,8 +26,35 @@ export default function Footer({}: Props) {
           target="_blank"
         />
       </div>
-      <p className=" text-xs text-gray-500 pb-2 tracking-[5px]">
-        From Corrientes to the world
+      <p className="text-xs pb-2 tracking-[5px]">
+        <span className="text-red-500">F</span>
+        <span className="text-orange-500">r</span>
+        <span className="text-yellow-500">o</span>
+        <span className="text-green-500">m</span>
+        <span className="text-blue-500"> </span>
+        <span className="text-indigo-500">C</span>
+        <span className="text-purple-500">o</span>
+        <span className="text-pink-500">r</span>
+        <span className="text-red-500">r</span>
+        <span className="text-orange-500">i</span>
+        <span className="text-yellow-500">e</span>
+        <span className="text-green-500">n</span>
+        <span className="text-blue-500">t</span>
+        <span className="text-indigo-500">e</span>
+        <span className="text-purple-500">s</span>
+        <span className="text-pink-500"> </span>
+        <span className="text-red-500">t</span>
+        <span className="text-orange-500">o</span>
+        <span className="text-yellow-500"> </span>
+        <span className="text-green-500">t</span>
+        <span className="text-blue-500">h</span>
+        <span className="text-indigo-500">e</span>
+        <span className="text-purple-500"> </span>
+        <span className="text-pink-500">w</span>
+        <span className="text-red-500">o</span>
+        <span className="text-orange-500">r</span>
+        <span className="text-yellow-500">l</span>
+        <span className="text-green-500">d</span>
       </p>
     </div>
   );
